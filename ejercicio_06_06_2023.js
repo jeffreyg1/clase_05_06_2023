93fb5a92c827df4e180bf11ac1199f30d26fb3f1
Jeffrey Andres Guette Sanguino <jeffrey.guette@fi-group.com>
2023-06-13 20:21:10 -0500

    Ejercicios_Switch

---
+++ ejercicio_06_06_2023.js
@@ -0,0 +1,237 @@
+//Ejercicio 1
+var dia=1;
+switch(dia){
+    case 1:
+    console.log("El dia es Lunes")
+        break;
+    case 2:
+    console.log("El dia es Martes")
+        break;
+    case 3:
+    console.log("El dia es Miercoles")
+        break;
+    case 4:
+    console.log("El dia es Jueves")
+        break;
+    case 5:
+    console.log("El dia es Viernes")
+        break;
+    case 6:
+    console.log("El dia es Sabado")
+        break;
+    case 7:
+    console.log("El dia es Domingo")
+        break;
+}
+
+//Ejercicio 2
+var n_mes=12;
+switch(n_mes){
+    case 1:
+    console.log("El mes es enero")
+        break;
+    case 2:
+    console.log("El mes es febrero")
+        break;
+    case 3:
+    console.log("El mes es marzo")
+        break;
+    case 4:
+    console.log("El mes es abril")
+        break;
+    case 5:
+    console.log("El mes es mayo")
+        break;
+    case 6:
+    console.log("El mes es junio")
+        break;
+    case 7:
+    console.log("El mes es julio")
+        break;
+    case 8:
+    console.log("El mes es agostoo")
+        break;
+    case 9:
+    console.log("El mes es septiembre")
+        break;
+    case 10:
+    console.log("El mes es octubre")
+        break;
+    case 11:
+    console.log("El mes es noviembre")
+        break;
+    case 12:
+    console.log("El mes es diciembre")
+        break;
+}
+
+//Ejercicio 3
+
+var n_mes="Enero";
+switch(n_mes){
+    case "Enero":
+    console.log("El numero del mes es 1")
+        break;
+    case "Febrero":
+    console.log("El numero del mes es 2")
+        break;
+    case "Marzo":
+    console.log("El numero del mes es 3")
+        break;
+    case "Abril":
+    console.log("El numero del mes es 4")
+        break;
+    case "Mayo":
+    console.log("El numero del mes es 5")
+        break;
+    case "Junio":
+    console.log("El numero del mes es 6")
+        break;
+    case "Julio":
+    console.log("El numero del mes es 7")
+        break;
+    case "Agosto":
+    console.log("El numero del mes es 8")
+        break;
+    case "Septiembre":
+    console.log("El numero del mes es 9")
+        break;
+    case "Octubre":
+    console.log("El numero del mes es 10")
+        break;
+    case "Noviembre":
+    console.log("El numero del mes es 11")
+        break;
+    case "Diciembre":
+    console.log("El numero del mes es 12")
+        break;
+    }
+
+//Ejercicio 4
+var nota=1;
+switch(nota){
+    case (nota=1):
+    console.log("Nota muy deficiente")
+        break;
+    case (nota=2):
+    console.log("Insuficiente")
+        break;
+    case (nota=3):
+    console.log("Suficiente")
+        break;
+    case (nota=4):
+    console.log("Bien")
+        break;
+    case (nota=5):
+    console.log("Notable")
+        break;
+    case (nota=6):
+    console.log("Sobresaliente")
+        break;
+}
+
+//Ejercicio 5 
+var n_mes="Enero";
+switch(n_mes){
+    case "Enero":
+    console.log("El numero dias de este mes es de 31 dias")
+        break;
+    case "Febrero":
+    console.log("El numero dias de este mes es de 28")
+        break;
+    case "Marzo":
+    console.log("El numero dias de este mes es de 31 dias")
+        break;
+    case "Abril":
+    console.log("El numero dias de este mes es de 30")
+        break;
+    case "Mayo":
+    console.log("El numero dias de este mes es de 31 dias")
+        break;
+    case "Junio":
+    console.log("El numero dias de este mes es de 30")
+        break;
+    case "Julio":
+    console.log("El numero dias de este mes es de 31 dias")
+        break;
+    case "Agosto":
+    console.log("El numero dias de este mes es de 31 dias")
+        break;
+    case "Septiembre":
+    console.log("El numero dias de este mes es de 30")
+        break;
+    case "Octubre":
+    console.log("El numero dias de este mes es de 31 dias")
+        break;
+    case "Noviembre":
+    console.log("El numero dias de este mes es de 30")
+        break;
+    case "Diciembre":
+    console.log("El numero dias de este mes es de 31 dias")
+        break;
+    }
+
+//Ejercicio 6
+var edad=33;
+switch(true){
+    case (edad>=0 && edad<=2):
+    console.log("Eres un bebe")
+      break;
+    case (edad>=3 && edad<=12):
+    console.log("Eres un niño/a")
+      break;
+    case (edad>=13 && edad<=19):
+    console.log("Eres un adolescente")
+      break;
+    case (edad>=20 && edad<=59):
+    console.log("Eres un adulto")
+      break;
+    case (edad>=60):
+    console.log("Eres un adulto mayor")
+        break;
+}
+
+//Ejercicio 7
+var magnitud=100
+var unidad_especificada="km"
+var unidad_deseada="km"
+switch(unidad_deseada){
+    case("cm"):
+    magnitud_nueva=100*10000
+    console.log("El valor de "+magnitud+" "+unidad_especificada+" a la unidad deseada es de "+magnitud_nueva+" "+unidad_deseada)
+    break;
+        case("m"):
+    magnitud_nueva=100*1000
+    console.log("El valor de "+magnitud+" "+unidad_especificada+" a la unidad deseada es de "+magnitud_nueva+" "+unidad_deseada)
+    break;
+        case("km"):
+    magnitud_nueva=100*1
+    console.log("El valor de "+magnitud+" "+unidad_especificada+" a la unidad deseada es de "+magnitud_nueva+" "+unidad_deseada)
+    break;
+}
+
+//Ejercicio 8
+var dia="sabado";
+switch(dia){
+    case "lunes":
+    console.log("El dia es laboral")
+        break;
+    case "martes":
+    console.log("El dia es laboral")
+        break;
+    case "miercoles":
+    console.log("El dia es laboral")
+        break;
+    case "jueves":
+    console.log("El dia es laboral")
+        break;
+    case "viernes":
+    console.log("El dia es laboral")
+        break;
+    case "sabado":
+    console.log("El dia es no laboral")
+        break;
+    case "Domingo":
+    console.log("El dia es no laboral")
+        break;
+}
